31b340010d23c01b7d4c725d2fd50aa46faedc43
Chris <chrissouth89@gmail.com>
2019-12-09 21:27:54 -0600

    finished loop practice

---
+++ w1d3_homework/loops.js
@@ -0,0 +1,20 @@
+for (let k=0; k <= 99; k++) {
+    console.log(k)
+}
+
+for (let i=99; i >= 0; i--) {
+    console.log(i)
+}
+
+for (let g=49; g <= 72; g++) {
+    console.log(g)
+}
+
+for (let p=81; p >= 26; p--) {
+    console.log(p)
+}
+
+for (let m=0; m <= 99; m++) {
+    if (m % 2 !== 0)
+    console.log(m)
+}
