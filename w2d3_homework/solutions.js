6af282ac98579d61f2669a9f330558baa9df0a1d
Chris <chrissouth89@gmail.com>
2019-12-17 18:20:12 -0600

    Sum Array Completed

--- w2d3_homework/solutions.js
+++ w2d3_homework/solutions.js
@@ -15,3 +15,13 @@ const checkPalindrome = (string) => {
 
 console.log(checkPalindrome("Radar"));
 console.log(checkPalindrome("Borscht"));
+///////////////////////////////////////////////////
+const sumArray = (arr) => {
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++) {
+        sum += arr[i]
+    }
+    return sum
+}
+console.log(sumArray([1, 2, 3, 4, 5, 6]));
+////////////////////////////////////////////////////
