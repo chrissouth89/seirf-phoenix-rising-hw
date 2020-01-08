49d5b7fefb3e051b6fafe5d6de9522c3039cb46e
Chris <chrissouth89@gmail.com>
2020-01-07 22:03:40 -0600

    combine objects, arrays, and functions

---
+++ w4d1_homework/combining_datatypes.js
@@ -0,0 +1,15 @@
+const crayonBox = ["Blue", "Green", "Red", "Purple"]
+// console.log(crayonBox[2])
+
+const bottle = {
+    cap: {material: "metal", color: "red", shape: "circle"}
+}
+// console.log(bottle.cap.color)
+
+const receipt = [
+    {name: "cereal", price: "$3"}
+]
+// console.log(receipt[0].name)
+
+const apartmentBuilding = ["a12", "a13", "a14"]
+console.log(apartmentBuilding)
