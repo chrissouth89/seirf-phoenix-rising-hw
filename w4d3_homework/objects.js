7e8cd2cbe87e8bc569f634e73d58c7583b43cb2f
Chris <chrissouth89@gmail.com>
2020-01-11 08:57:29 -0600

    created hamster class

---
+++ w4d3_homework/objects.js
@@ -0,0 +1,19 @@
+class Hamster {
+    constructor (name) {
+    this.owner = "";
+    this.name = name;
+    this.price = 15;
+}
+
+wheelRun() {
+    console.log("Squeek Squeek");
+}
+
+eatFood() {
+    console.log("Nibble Nibble");
+}
+
+getPrice() {
+    return this.getPrice
+}
+};
