23a6d20dafaeb4620d994753b2ef1d485f216787
Chris <chrissouth89@gmail.com>
2020-01-13 14:34:39 -0600

    dougie and pizza rat have fought!

--- w4d3_homework/donut_adventure/adventure.js
+++ w4d3_homework/donut_adventure/adventure.js
@@ -1,25 +1,86 @@
 // console.log("Hello World!")
 
 class Hero {
-    constructor(name) {
-        this.name = name;
-        this.health = 100;
-        this.weapons = { sprinkleSpray: 5, sugarShock: 10 }
-        this.catchPhrases = [`I\'m fresher than day old pizza`, 
-        `You can\'t count my calories`]
-    } 
-    randomNumGen() {
-        return Math.floor(Math.random() * this.catchPhrases.length)
-    }
-    talkSass() {
-        console.log(this.catchPhrases[this.randomNumGen(this.catchPhrases)]);
-    }
-    announceHealth() {
-        console.log(`I have ${this.health} health left!`)
-    }
-    fight() {
-        console.log(`I\'m ready to rumble!`)
-    }
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    this.weapons = [
+      {
+        name: "SprinkleSpray",
+        damage: 5
+      },
+      {
+        name: "SugarShock",
+        damage: 10
+      }
+    ];
+    this.catchPhrases = [
+      `I\'m fresher than day old pizza`,
+      `You can\'t count my calories`
+    ];
+  }
+  randomNumGen(arr) {
+    return Math.floor(Math.random() * arr.length);
+  }
+  talkSass() {
+    console.log(this.catchPhrases[this.randomNumGen(this.catchPhrases)]);
+  }
+  announceHealth() {
+    console.log(
+      `I have ${this.health} health left! Why would I announce this to you?!`
+    );
+  }
+  randomWeapon() {
+    return this.weapons[this.randomNumGen(this.weapons)];
+  }
+  fight(enemy) {
+    let weapon = this.randomWeapon();
+    enemy.health -= weapon.damage;
+    console.log(`${weapon.name} does ${weapon.damage} damage to ${enemy.name}`);
+  }
 }
 const dougie = new Hero(`Dougie the Donut`);
 
+class Enemy {
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    this.weapons = [
+      { name: "Pepperoni Stars", damage: 5, name: "Cheese Grease", damage: 10 }
+    ];
+    this.catchPhrases = [
+      `I\'m Youtube famous`,
+      `I\'m more dangerous than an uncovered sewer`
+    ];
+  }
+  ranNumGen(arr) {
+    return Math.floor(Math.random() * arr.length);
+  }
+  talkSmack() {
+    console.log(this.catchPhrases[this.ranNumGen(this.catchPhrases)]);
+  }
+  announceHealth() {
+    console.log(
+      `I have ${this.health} health left! Why am I telling you this?!`
+    );
+  }
+  randomWeapon() {
+    return this.weapons[this.ranNumGen(this.weapons)];
+  }
+  fight(enemy) {
+    let weapon = this.randomWeapon();
+    enemy.health -= weapon.damage;
+    console.log(`${weapon.name} does ${weapon.damage} damage to ${enemy.name}`);
+  }
+}
+
+const rat = new Enemy(`Pizza Rat`);
+
+// console.log(dougie.talkSass())
+// console.log(rat.talkSmack())
+// console.log(dougie.announceHealth())
+
+console.log(rat.fight(dougie))
+console.log(dougie.fight(rat));
+console.log(dougie.announceHealth())
+console.log(rat.announceHealth())
