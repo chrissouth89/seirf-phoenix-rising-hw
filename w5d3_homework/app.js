90e0aca4d6efc21338c2f904e038f86567521127
Chris <chrissouth89@gmail.com>
2020-01-19 14:28:10 -0600

    i added my first element and it was fun

--- w5d3_homework/app.js
+++ w5d3_homework/app.js
@@ -11,12 +11,12 @@ if (typeof jQuery === "undefined"){
     console.log("oops, I still have to link my jQuery properly!");
 } else {console.log("I did it! I linked jQuery and this js file!")};
 
-
-
-
-
-
-
+const $container = $('#container') 
+console.log($container)
+const $h1 = $('<h1>')
+console.log($h1)
+$h1.text('Hello World!')
+$container.append($h1)
 
 
   });
