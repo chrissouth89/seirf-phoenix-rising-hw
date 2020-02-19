43a5b7d31e8c0b81716a72c0a1e2b3b56aa026e5
Chris <chrissouth89@gmail.com>
2020-02-18 18:18:30 -0600

    completed homework

---
+++ w9d3_hw/pokemon_app/models/pokemon.js
@@ -0,0 +1,14 @@
+const pokemon = [
+    {name: "bulbasaur", img: "http://img.pokemondb.net/artwork/bulbasaur"},
+    {name: "ivysaur", img: "http://img.pokemondb.net/artwork/ivysaur"},
+    {name: "venusaur", img: "http://img.pokemondb.net/artwork/venusaur"},
+    {name: "charmander", img: "http://img.pokemondb.net/artwork/charmander"},
+    {name: "charmeleon", img: "https://img.pokemondb.net/artwork/charmeleon"},
+    {name: "charizard", img: "http://img.pokemondb.net/artwork/charizard"},
+    {name: "squirtle", img: "http://img.pokemondb.net/artwork/squirtle"},
+    {name: "wartortle", img: "http://img.pokemondb.net/artwork/wartortle"},
+    {name: "blastoise", img: "https://img.pokemondb.net/artwork/blastoise"}
+
+ ];
+
+ module.exports = pokemon;
---
+++ w9d3_hw/pokemon_app/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 3000;
+
+const pokemon = require('./models/pokemon.js')
+
+app.get('/pokemon/:id', (req, res) => {
+    res.render('show.ejs', { pokemon: pokemon[req.params.id]})
+})
+
+app.get('/pokemon', (req, res) => {
+    res.render('index.ejs', { pokemon: pokemon})
+})
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Pokemon App!')
+})
+
+app.listen(port, () => {
+    console.log('serving up Pokemon! on port ', port)
+})
